7f188c95f55a4ab8f2cf3cdbd25744e2154d0bae
CoralieNeubueser <coralie.neubuser@cern.ch>
2019-03-06 16:45:23 +0100

    one calo block

--- src/B4DetectorConstruction.cc
+++ src/B4DetectorConstruction.cc
@@ -342,11 +342,11 @@ G4VPhysicalVolume* B4DetectorConstruction::DefineVolumes()
 {
 	// Geometry parameters
         auto caloThickness = 250*cm;  
-	const G4int numLayers = 2;
+	const G4int numLayers = 1;
 	G4int granularity = 1;
 
 	calorSizeXY  = 100*cm;
-	auto firstLayerThickness=25*cm;
+	auto firstLayerThickness=0*cm;
 
 	G4double absorberFraction=1e-6;	
 
